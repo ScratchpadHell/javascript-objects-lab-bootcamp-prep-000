e06d36d2786e509493aa1e25979bef948172c90f
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-23 20:10:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = new Object; 
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value} )
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj; 
+}
+function deleteFromObjectByKey(obj, key)
+
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj.key;
+}
